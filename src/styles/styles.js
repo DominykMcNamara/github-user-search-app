2c3176defe51819e2d9a81ac314513cf8cd6fe40
DominykMcNamara <dominyksmith@gmail.com>
2022-08-15 23:44:52 -0400

    Configure Dark Theme

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -93,3 +93,97 @@ export const lightTheme = createTheme({
     },
   },
 });
+
+export const darkTheme = createTheme({
+  breakpoints: {
+    values: {
+      mobile: 0,
+      tablet: 768,
+      laptop: 1024,
+      desktop: 1200,
+    },
+  },
+  palette: {
+    mode: "dark",
+    common: {
+      white: "#FEFEFE",
+      black: "#2B3442",
+    },
+    primary: {
+      main: "#FFFFFF",
+    },
+    secondary: {
+      main: "#F6F8FF",
+    },
+    action: {
+      main: "#0079FF",
+    },
+    info: {
+      main: "#1E2A47",
+    },
+  },
+  typography: {
+    fontFamily: '"Space Mono", "Helvetica", sans-serif',
+    h1: {
+      fontFamily: '"Space Mono", "Helvetica", sans-serif',
+      fontWeight: 700,
+      fontSize: "1.6rem",
+      lineHeight: "2.3rem",
+    },
+    h2: {
+      fontFamily: '"Space Mono", "Helvetica", sans-serif',
+      fontWeight: 700,
+      fontSize: "1.3rem",
+      lineHeight: "2rem",
+    },
+    h3: {
+      fontFamily: '"Space Mono", "Helvetica", sans-serif',
+      fontWeight: 400,
+      fontSize: "1rem",
+      lineHeight: "1.5rem",
+    },
+    h4: {
+      fontFamily: '"Space Mono", "Helvetica", sans-serif',
+      fontWeight: 400,
+      fontSize: "0.8rem",
+      lineHeight: "1.2rem",
+    },
+    body1: {
+      fontFamily: '"Space Mono", "Helvetica", sans-serif',
+      fontWeight: 400,
+      fontSize: "0.9rem",
+      lineHeight: "1.5rem",
+    },
+  },
+  components: {
+    MuiInput: {
+      styleOverrides: {
+        root: {
+          fontFamily: '"Space Mono", "Helvetica", sans-serif',
+          fontWeight: 400,
+          fontSize: "1.1rem",
+          lineHeight: "1.5rem",
+          borderRadius: "0.9rem",
+          boxShadow: "0px 16px 30px -10px rgba(70, 96, 187, 0.198567)",
+          backgroundColor: "info"
+        },
+      },
+      defaultProps: {
+        color: "primary",
+        disableUnderline: true,
+      },
+    },
+    MuiLink: {
+      styleOverrides: {
+        root: {
+          lineHeight: "1.3rem",
+        },
+      },
+      defaultProps: {
+        color: "primary",
+        underline: "hover",
+        variant: "body1",
+      },
+    },
+  },
+});
