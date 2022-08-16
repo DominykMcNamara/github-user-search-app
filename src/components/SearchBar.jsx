af3d2af128b1534e17982565c61e82516bcb1df3
DominykMcNamara <dominyksmith@gmail.com>
2022-08-16 17:38:32 -0400

    Created a user state

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,20 +4,27 @@ import { Box } from "@mui/system";
 import { Button, IconButton, InputBase } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import { SearchButton } from "./SearchButton";
+
 export const SearchBar = ({ theme }) => {
+  const [user, setUser] = useState();
+
+  const handleUserChange = (e) => {
+    setUser(e.target.value);
+  };
+
   return (
     <Box>
       <InputBase
         fullWidth={true}
         placeholder="Search GitHub username..."
+        value={user}
+        onChange={handleUserChange}
         startAdornment={
           <IconButton>
             <SearchIcon sx={{ mr: "2rem" }} />
           </IconButton>
         }
-        endAdornment={
-         <SearchButton />
-        }
+        endAdornment={<SearchButton />}
         sx={{
           backgroundColor: theme.palette.common.white,
           height: "4rem",
